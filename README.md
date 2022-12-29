69815108ae8d9aab24b6214f33eff5a0e33ecff0
cheeter_Lee <johnson991210@gmail.com>
2022-12-29 10:50:25 +0800

    overview and geography

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# data-dashboard
+# cheetDash: A Data visualizing dashboard
+
+Powered by **MongoDB**, **Express**, **React** and **Node**, styled with **MaterialUI**
--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import morgan from 'morgan'
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import helmet from "helmet"
+import morgan from "morgan"
 import clientRoutes from "./routes/client.js"
 import generalRoutes from "./routes/general.js"
 import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
 // data imports
-import User from './models/User.js'
-import Product from './models/Product.js'
-import ProductStat from './models/ProductStat.js'
-import Transaction from './models/Transaction.js'
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js'
+import User from "./models/User.js"
+import Product from "./models/Product.js"
+import ProductStat from "./models/ProductStat.js"
+import Transaction from "./models/Transaction.js"
+import {
+	dataUser,
+	dataProduct,
+	dataProductStat,
+	dataTransaction,
+	dataOverallStat,
+} from "./data/index.js"
+import OverallStat from "./models/OverallStat.js"
 
 // Configuration
 dotenv.config()
@@ -36,21 +43,23 @@ app.use("/sales", salesRoutes)
 
 // Mongoose setup
 const PORT = process.env.PORT || 9000
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Connected to db and listening on Port: ${PORT}`)
-            
-            // Insert mock data
-            // User.insertMany(dataUser)
-            // Product.insertMany(dataProduct)
-            // ProductStat.insertMany(dataProductStat)
-            // Transaction.insertMany(dataTransaction)
-        })
-    })
-    .catch((error) => {
-        console.log(`${error}, did not connect`)
-    })
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Connected to db and listening on Port: ${PORT}`)
+
+			// Insert mock data
+			// User.insertMany(dataUser)
+			// Product.insertMany(dataProduct)
+			// ProductStat.insertMany(dataProductStat)
+			// Transaction.insertMany(dataTransaction)
+			// OverallStat.insertMany(dataOverallStat)
+		})
+	})
+	.catch((error) => {
+		console.log(`${error}, did not connect`)
+	})
